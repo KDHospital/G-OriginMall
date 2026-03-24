80b941baa1cdbc008552deb778ea1bcf8a4fbe74
jaeyoungu <dignak123@gmail.com>
2026-03-24 12:11:47 +0900

    feat: 서비스 추가

---
+++ back/src/main/java/com/example/gmall/service/EmailService.java
@@ -0,0 +1,16 @@
+package com.example.gmall.service;
+
+public interface EmailService {
+
+	
+	// 이메일 인증코드 발송
+	// POST /api/member/email/send
+	//@param email 인증코드를 받을 이메일 주소
+	void sendCode(String email);
+	
+	//이메일 인증코드 확인
+	//POST /api/member/email/verify
+	// @param email 인증 요청 이메일
+	// @param code 사용자가 입력한 6자리 코드
+	void verifyCode(String email, String code);
+}
