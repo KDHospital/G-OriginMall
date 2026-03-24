fec7f539643be2fb938dea016cf713482b417eee
“LHJ921204” <xhzhdb@gmail.com>
2026-03-24 14:14:48 +0900

    feat: product Repository 생성

---
+++ back/src/main/java/com/example/gmall/repository/ProductRepository.java
@@ -0,0 +1,16 @@
+package com.example.gmall.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.example.gmall.domain.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+	//전체 상품 목록, ACTIVE 상태만 조회
+	@Query("SELECT p FROM Product p JOIN FETCH p.category"+"WHERE p.soldStatus = 0"+"AND (:categoryId IS NULL OR p.category.categoryId = :categoryId)")
+	Page<Product> findActiveProducts(@Param("categoryId") Integer categoryId,Pageable pageable);
+
+}
