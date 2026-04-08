82cde9ea5f85973e4e381fe03b706d0d0574d33f
jaeyoungu <dignak123@gmail.com>
2026-04-08 16:48:05 +0900

    fix: 알림이 안뜨는 버그 수정

--- front/src/components/ProtectedRoute.jsx
+++ front/src/components/ProtectedRoute.jsx
@@ -1,20 +1,40 @@
 import { Navigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 
 const ProtectedRoute = ({children,allowRole}) => {
 
     const useInfo = JSON.parse(localStorage.getItem("member"))
+    const [redirect, setRedirect] = useState(null);
 
-    if(!useInfo) {
-        alert("로그인이 필요한 서비스입니다.")
-        return <Navigate to={"/login"} replace />
-    }
+    useEffect(()=>{
 
-    if(allowRole !== undefined && useInfo.role != allowRole) {
-        alert("접근 권한이 없습니다.")
-        return <Navigate to={"/"} replace />
-    }
+        if(!useInfo) {
+            setTimeout(()=>{
+            alert("로그인이 필요한 서비스입니다.")
+            setRedirect("/login");
+            },500)
+            
+        }
+
+        if(allowRole !== undefined && useInfo.role != allowRole) {
+            setTimeout(()=>{
+            alert("접근 권한이 없습니다.")
+            setRedirect("/")
+
+            },500)
+
+        }
+        
+        
+
+    }, []);
+
+    if (redirect) return <Navigate to={redirect} replace />;
+    if (!useInfo) return null;
+    if (allowRole !== undefined && useInfo.role != allowRole) return null;
+
+    return children;
     
-    return children
 }
 export default ProtectedRoute
