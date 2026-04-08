31eeef33bd7caf8a8c8e8357c7269ca5254d1a4e
jaeyoungu <dignak123@gmail.com>
2026-04-08 17:45:57 +0900

    fix:로그인페이지로 이동안하던 버그 수정

--- front/src/components/ProtectedRoute.jsx
+++ front/src/components/ProtectedRoute.jsx
@@ -1,40 +1,52 @@
 import { Navigate } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 
 const ProtectedRoute = ({children,allowRole}) => {
 
-    const useInfo = JSON.parse(localStorage.getItem("member"))
-    const [redirect, setRedirect] = useState(null);
+    const userInfo = JSON.parse(localStorage.getItem("member"))
+    const [shouldRedirect, setShouldRedirect] = useState(false)
+    const [redirectPath,setRedirectPath] = useState("")
+    const [isVerified, setIsVerified] = useState(false)
+    const hasAlerted = useRef(false)
 
-    useEffect(()=>{
-
-        if(!useInfo) {
-            setTimeout(()=>{
-            alert("로그인이 필요한 서비스입니다.")
-            setRedirect("/login");
-            },500)
-            
-        }
-
-        if(allowRole !== undefined && useInfo.role != allowRole) {
-            setTimeout(()=>{
-            alert("접근 권한이 없습니다.")
-            setRedirect("/")
-
-            },500)
-
-        }
-        
-        
-
-    }, []);
-
-    if (redirect) return <Navigate to={redirect} replace />;
-    if (!useInfo) return null;
-    if (allowRole !== undefined && useInfo.role != allowRole) return null;
-
-    return children;
+useEffect(() =>{
+    if(hasAlerted.current) return
     
+    if(!userInfo) {
+        setTimeout(() => {
+              hasAlerted.current = true
+        alert("로그인이 필요한 서비스입니다.")
+        setRedirectPath("/login")
+        setShouldRedirect(true)
+        }, 500);
+      
+        
+        
+    }else if(allowRole !== undefined && userInfo.role != allowRole) {
+        setTimeout(() => {
+            hasAlerted.current = true
+        alert("접근 권한이 없습니다.")
+        setRedirectPath("/")
+        setShouldRedirect(true)
+        }, 500);
+        
+    } else{
+        setIsVerified(true)
+    }
+},[userInfo,allowRole])
+
+if(shouldRedirect) {
+    return <Navigate to={redirectPath} replace />
+}
+
+if(!userInfo && !shouldRedirect) return null
+
+if(allowRole !== undefined && userInfo?.role != allowRole && shouldRedirect) return null
+
+if(!isVerified){
+    return  null
+}
+    return children
 }
 export default ProtectedRoute
