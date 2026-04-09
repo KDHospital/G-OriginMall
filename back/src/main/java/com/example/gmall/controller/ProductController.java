2e7635c0c78867c5347a71d74e842c93ccddf1a4
arculmanager777 <arculmanager777@gmail.com>
2026-04-09 15:40:33 +0900

    "feat" 사용하지 않은 import 정리

--- back/src/main/java/com/example/gmall/controller/ProductController.java
+++ back/src/main/java/com/example/gmall/controller/ProductController.java
@@ -12,16 +12,15 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.gmall.domain.Product;
-import com.example.gmall.dto.category.CategoryResponseDTO;
 import com.example.gmall.dto.product.ProductDetailResponseDTO;
 import com.example.gmall.dto.product.ProductListResponseDTO;
 import com.example.gmall.dto.product.ProductRequestDTO;
