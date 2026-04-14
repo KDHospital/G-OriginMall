a13b4bb370cb145680c9d404e0974a4f43fd9cb5
jaeyoungu <dignak123@gmail.com>
2026-04-14 12:36:23 +0900

    fix:마이페이지에 주문내역과 배송지관리에서 회원정보가 아나오는 버그 수정

--- front/src/pages/member/MyAddresses.jsx
+++ front/src/pages/member/MyAddresses.jsx
@@ -8,6 +8,7 @@ import AddressSection from "../../components/member/AddressSection";
 export default function MyAddresses() {
     const navigate = useNavigate();
     const [addresses, setAddresses] = useState([]);
+    const [memberInfo, setMemberInfo] = useState(null)
 
     useEffect(() => {
         const savedMember = localStorage.getItem("member");
@@ -16,6 +17,8 @@ export default function MyAddresses() {
             navigate("/login");
             return;
         }
+        const parsedMember = JSON.parse(savedMember);
+        setMemberInfo(parsedMember);
 
         axiosInstance.get("/members/addresses")
             .then((res) => setAddresses(res.data))
@@ -25,10 +28,10 @@ export default function MyAddresses() {
     return (
         <BasicLayout>
             <div className="max-w-7xl mx-auto flex gap-8 p-10 bg-gray-50 min-h-screen">
-                <MyPageComponent member={null} />
+                {memberInfo && <MyPageComponent member={memberInfo} />}
 
                 <main className="flex-grow space-y-6">
-                    <h2 className="text-2xl font-bold border-l-4 border-black pl-3">배송지 관리</h2>
+                    <h2 className="text-2xl font-bold border-l-4 border-green-600 pl-3">배송지 관리</h2>
                     <AddressSection
                         addresses={addresses}
                         showSelection={false}
