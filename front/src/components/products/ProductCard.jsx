a7493a44648ac11c1657d6ad4d16c2f96ecbdc66
“LHJ921204” <xhzhdb@gmail.com>
2026-04-20 14:25:01 +0900

    Feat: 상품리스트에서 기획전 뱃지 보이기

--- front/src/components/products/ProductCard.jsx
+++ front/src/components/products/ProductCard.jsx
@@ -28,8 +28,14 @@ const ProductCard = ({item}) => {
                 </div>
             )}
             <div className="space-y-1 px-1">
-                <h3 className="font-manrope font-bold text-lg text-on-surface">{item.pname}</h3>
-                <p className="font-manrope text-xs text-on-surface-variant line-clamp-1 italic">{item.categoryName}</p>
+                <h3 className="font-manrope font-bold text-lg text-on-surface truncate">{item.pname}</h3>
+                <div className="flex justify-between">
+                    <p className="font-manrope text-xs text-on-surface-variant line-clamp-1 italic">{item.categoryName}</p>
+                    <div className="flex items-center items-center">
+                        {item.certified ? <span className={`text-xs px-2 py-1 mr-1 rounded-full font-medium bg-yellow-100 text-yellow-600}`}>금빛나루 인증</span>: <></>}
+                        {item.exhibition ? <span className={`text-xs px-2 py-1 rounded-full font-medium bg-green-100 text-green-600}`}>기획전 상품</span>: <></>}
+                    </div>
+                </div>
                 <div className="flex items-center gap-2 pt-2">
                     <span className="font-manrope font-extrabold text-primary text-xl tracking-tight">{(item.price||0).toLocaleString()}원</span>
                     <span className="font-manrope text-sm text-outline line-through">{item.listPrice.toLocaleString()}원</span>
